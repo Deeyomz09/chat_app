962c72fcb8d50643fc901837a5394f833f198572
Deeyomz <diomedesjrco@gmail.com>
2024-06-13 12:11:46 +0800

    Added database

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+require("dotenv").config();
+const database = process.env.MONGO_URI;
+
+const connectDatabase = async () => {
+  try {
+    await mongoose.connect(database);
+    console.log("MongoDB is connected.");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDatabase;
--- server.js
+++ server.js
@@ -1,10 +1,17 @@
 const express = require("express");
+const databaseConnect = require("./config/database");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Middleware
+app.use(cors());
 app.use(express.json());
+
+// MongoDB Connection
+databaseConnect();
 
 app.get("/", (req, res) => {
   res.send("<h1>Chat APP</h1>");
