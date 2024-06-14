d50016bca7e723cf2379f72a40075affd3bd1b6c
Deeyomz <diomedesjrco@gmail.com>
2024-06-14 15:28:21 +0800

    Fix sending message and Conversation

--- models/user.model.js
+++ models/user.model.js
@@ -1,34 +1,37 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
-    required: true
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true
+    },
+    lastName: {
+      type: String,
+      required: true
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true,
+      minLegth: 6
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"]
+    },
+    profilePic: {
+      type: String,
+      default: ""
+    }
   },
-  lastName: {
-    type: String,
-    required: true
-  },
-  username: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  password: {
-    type: String,
-    required: true,
-    minLegth: 6
-  },
-  gender: {
-    type: String,
-    required: true,
-    enum: ["male", "female"]
-  },
-  profilePic: {
-    type: String,
-    default: ""
-  }
-});
+  { timestamps: true }
+);
 
 const User = mongoose.model("User", userSchema);
 
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import databaseConnect from "./config/database.js";
 import authRoutes from "./routes/auth.routes.js";
-import cors from "cors";
-import ChatMessage from "./models/ChatMessage.js";
+import messageRoutes from "./routes/message.routes.js";
 
 const app = express();
 const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 // Middleware
-app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
 app.listen(PORT, () => {
   // MongoDB Connection
